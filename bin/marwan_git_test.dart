bcc92ec7fed9e18915d24bf4737673ad0b4dc234
marwan ali <MarwanBinAbdullah0@gmail.com>
2024-12-04 16:32:57 +0300

    marwan test githup

---
+++ bin/marwan_git_test.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  print(sum(1, 4));
+}
+
+int sum(int x, int y) {
+  return x + y;
+}
---
+++ test/marwan_git_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:marwan_git_test/marwan_git_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
